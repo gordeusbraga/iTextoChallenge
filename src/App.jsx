9daeda52342da10258c71885bb4fa0582f5f0772
Gordeus <97854964+gordeusbraga@users.noreply.github.com>
2025-05-19 20:59:53 -0300

    Feature: using tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Header from "./components/Header.jsx";
+import "./styles/App.css";
 export default function App() {
   return (
-    <div>
+    <div className="w-full h-screen bg-black text-white">
       <Header></Header>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,10 @@
 import React from "react";
+import "../styles/App.css";
 
-
-export default props => {
-
-    return (
-        <header>
-            <h1 >/Dev/All</h1>
-
-        </header>
-    );
-}
+export default (props) => {
+  return (
+    <header className="flex items-center justify-between p-4 colors-gray-800">
+      <h1 className="text-4xl font-bold text-yellow-400">/Dev/All</h1>
+    </header>
+  );
+};
