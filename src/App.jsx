3af1b09731c39628fd9a05fc9c18b6e7a22ecfed
gordeusbraga <mb326780@gmail.com>
2025-05-19 20:04:22 -0300

    Configure tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import Header from "./components/Header.jsx";
 export default function App() {
   return (
     <div>
-
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default props => {
+
+    return (
+        <header>
+            <h1 >/Dev/All</h1>
+
+        </header>
+    );
+}
