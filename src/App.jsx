88c35e78ff8d6f8c5b85bf8e05c134de9486177c
gordeusbraga <mb326780@gmail.com>
2025-05-20 22:16:20 -0300

    Feature: Create PostList

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from "./components/Header.jsx";
 import Search from "./components/Search.jsx";
+import Posts from "./components/Posts.jsx";
 import "./styles/App.css";
 export default function App() {
   return (
-    <div className="w-screen h-screen bg-[#F9FAFB">
+    <section className="w-screen h-screen bg-[#F9FAFB] flex flex-col items-center justify-start ">
       <Header></Header>
       <Search></Search>
-    </div>
+      <section className="w-[100%] h-[80%] flex align-center justify-center flex-wrap overflow-y-scroll scrollbar-hide max-w-[1040px]">
+        <Posts />
+        <Posts />
+        <Posts />
+
+      </section>
+
+    </section>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Menu } from "lucide-react";
 import "../styles/App.css";
 
 export default (props) => {
-  return (
-    <header className="flex items-start justify-content w-full h-1/6 border-b-3 0border-[#111827] p-6">
-      <h1 className="text-[#14B605] text-6xl font-bold ml-7">/Dev/All</h1>
-    </header>
-  );
+    return (
+        <header className="flex items-center w-screen h-1/6 border-b-2 border-[#111827] justify-between mb-4"  >
+            <h1 className="text-[#14B605] text-5xl font-bold pl-6"  >/Dev/All</h1>
+            <button className="pt-1.5 pr-3"><Menu size={50} /></button>
+        </header>
+    );
 };
