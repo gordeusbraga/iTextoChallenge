8cb48c0b8c860b5e47b2c928a39a190a2d0a06ef
gordeusbraga <mb326780@gmail.com>
2025-05-21 09:47:41 -0300

    Fix: Add font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Menu } from "lucide-react";
 import "../styles/App.css";
 
+
 export default (props) => {
     return (
         <header className="flex items-center w-screen h-1/6 border-b-2 border-[#111827] justify-between mb-4"  >
-            <h1 className="text-[#14B605] text-5xl font-bold pl-6"  >/Dev/All</h1>
+            <h1 className="text-[#14B605] text-5xl  pl-6 font-title font-bold"  >/dev/All</h1>
             <button className="pt-1.5 pr-3"><Menu size={50} /></button>
         </header>
     );
